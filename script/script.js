e2ca5a780761304e14dcadd4dedb1fd418511148
pb-bennett <pb.bennett.uk@gmail.com>
2022-04-05 09:57:27 +0200

    added event listner to button container

--- script/script.js
+++ script/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const mainDisplay = document.getElementById('main-display');
+const upperDisplay = document.getElementById('upper-display');
+const btnNumberAll = document.querySelectorAll('btn-num');
+const btnAll = document.querySelectorAll('.btn');
+
+btnAll.forEach(el =>
+  el.addEventListener('click', function () {
+    console.log(el.dataset.btn);
+  })
+);
